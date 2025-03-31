6529140070932fb68b87e1e1b7d74c2867dffde0
petrikuittinen <petri.kuittinen@hamk.fi>
2025-03-31 11:10:14 +0300

    hello tutorial fully done

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Message
+from django.views.generic.list import ListView
 
-def index(request):
-    context = {"secret": "kakkulapio"}
-    return render(request, 'hello/index.html', context)
+# Create your views here.
+
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Message
+
+# def index(request):
+#     context = {"age": 25, "names": ["Jack", "Alice", "Jill"]}
+#     return render(request, 'hello/index.html', context)
+
+# def index(request):
+#     context = {"latest_message_list": Message.objects.order_by('-date')[:10]}
+#     return render(request, 'hello/index.html', context)
+
+def second(request):
+    context = {}
+    return render(request, 'hello/second.html', context)
+
+class IndexView(ListView):
+    template_name = 'hello/index.html'
+    context_object_name = 'latest_message_list'
+
+    def get_queryset(self):
+        """Return the last 10 published messages."""
+        return Message.objects.order_by('-date')[:10]
